36b5afa087d90c3452e2eeefa7bc3c5c9a4b1529
Manuel Lorenz <mlorenz@mlorenz-macbook.local>
2018-02-13 13:51:36 +0100

    added display for stats in loadout

--- src/components/stats/stats.ts
+++ src/components/stats/stats.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'stats',
   templateUrl: 'stats.html'
 })
 export class StatsComponent {
+
+  @Input()
+  private stats: any;
 
   constructor() {
   }
--- src/pages/loadout/loadout.ts
+++ src/pages/loadout/loadout.ts
@@ -13,16 +13,56 @@ export class LoadoutPage {
   loadout: any = {};
   isAndroid: boolean = false;
   private skills: any[];
+  private stats: any;
 
   constructor(public events: Events, private platform: Platform, public navCtrl: NavController, public navParams: NavParams) {
     this.isAndroid = platform.is('android');
     this.loadout = {
       weapon: {value: 'Weapon I', id: 1},
-      head: {name: 'Head I', id: 1, skills: []},
-      chest: {name: 'Chest I', id: 1, skills: []},
-      arms: {name: 'Arms I', id: 1, skills: []},
-      waist: {name: 'Waist I', id: 1, skills: []},
-      legs: {name: 'Legs I', id: 1, skills: []},
+      head: {
+        name: 'Head I', id: 1, skills: [], "fire": 1,
+        "ice": 14,
+        "thunder": -3,
+        "water": 0,
+        "dragon": 0,
+        "defense": 70
+      },
+      chest: {
+        name: 'Chest I', id: 1, skills: [],
+        "fire": 1,
+        "ice": 14,
+        "thunder": -3,
+        "water": 0,
+        "dragon": 0,
+        "defense": 70
+      },
+      arms: {
+        name: 'Arms I', id: 1, skills: [],
+        "fire": 1,
+        "ice": 14,
+        "thunder": -3,
+        "water": 0,
+        "dragon": 0,
+        "defense": 70
+      },
+      waist: {
+        name: 'Waist I', id: 1, skills: [],
+        "fire": 1,
+        "ice": 14,
+        "thunder": -3,
+        "water": 0,
+        "dragon": 0,
+        "defense": 70
+      },
+      legs: {
+        name: 'Legs I', id: 1, skills: [],
+        "fire": 1,
+        "ice": 14,
+        "thunder": -3,
+        "water": 0,
+        "dragon": 0,
+        "defense": 70
+      },
       charm: {name: 'Charm I', id: 1, skills: []}
     };
     this.subscribeToAllEvents();
@@ -64,16 +104,36 @@ export class LoadoutPage {
 
   calcSkills() {
     this.skills = this.buildSkillList();
-    this.skills = _.chain(this.skills).groupBy('skillId').map(function(skills, key) {
+    this.skills = _.chain(this.skills).groupBy('skillId').map(function (skills, key) {
       return {
         name: skills[0].name,
         skillId: skills[0].skillId,
-        level: _(skills).reduce(function(m,x) {
+        level: _(skills).reduce(function (m, x) {
           return m + x.level;
         }, 0)
       }
     }).value();
-    console.log(this.skills);
   }
 
+  calcStats() {
+    this.stats =
+      {
+        attack: this.loadout.weapon.attack,
+        defense: this.calcSingleStat('defense'),
+        fire: this.calcSingleStat('fire'),
+        ice: this.calcSingleStat('ice'),
+        thunder: this.calcSingleStat('thunder'),
+        dragon: this.calcSingleStat('dragon'),
+        water: this.calcSingleStat('water')
+      };
+    console.log(this.stats)
+  }
+
+  calcSingleStat(type) {
+    return this.loadout.chest[type]
+      + this.loadout.head[type]
+      + this.loadout.arms[type]
+      + this.loadout.legs[type]
+      + this.loadout.waist[type];
+  }
 }
